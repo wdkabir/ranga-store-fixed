7601284be2c618516fa000ab70e3d17a320012eb
wdkabir <wdkabirhossain@gmail.com>
2021-09-14 23:56:10 +0600

    last

--- js/app.js
+++ js/app.js
@@ -9,19 +9,27 @@ const loadProducts = () => {
 const showProducts = (products) => {
   const allProducts = products.map((pd) => pd);
   for (const product of allProducts) {
-    const image = product.images;
-    const div = document.createElement("div");
-    div.classList.add("product");
-    div.innerHTML = `<div class="single-product">
-      <div>
-    <img class="product-image" src=${image}></img>
-      </div>
-      <h3>${product.title}</h3>
-      <p>Category: ${product.category}</p>
-      <h2>Price: $ ${product.price}</h2>
-      <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="buy-now btn btn-success">add to cart</button>
-      <button id="details-btn" class="btn btn-danger">Details</button></div>
-      `;
+    const image = product.image;
+    const div = document.createElement('div')
+    div.classList.add("col");
+    div.innerHTML = `
+            <div class="card h-100">
+                <div class="p-3">
+                    <img class="card-img-top product-image" src=${image}></img>
+                </div>
+                <div class="card-body">
+                <h5 class="card-title">${product.title}</h5>
+                <h6>Category: ${product.category}</h6>
+                <h5>Price: $${product.price}</h5>
+                </div>
+                <div class="card-footer text-center">
+                  <div class="d-flex flex-column flex-md-row justify-content-between">
+                    <button onclick="addToCart(${product.id},${product.price})" id="addToCart-btn" class="btn btn-outline-primary fw-bold mb-2">Add to cart</button>
+                    <button id="details-btn" class="btn btn-outline-info fw-bold mb-2">Details</button>
+                  </div>
+                </div>
+            </div>     
+        `;
     document.getElementById("all-products").appendChild(div);
   }
 };
@@ -29,14 +37,14 @@ let count = 0;
 const addToCart = (id, price) => {
   count = count + 1;
   updatePrice("price", price);
-
   updateTaxAndCharge();
+  updateTotal();
   document.getElementById("total-Products").innerText = count;
 };
 
 const getInputValue = (id) => {
   const element = document.getElementById(id).innerText;
-  const converted = parseInt(element);
+  const converted = parseFloat(element);
   return converted;
 };
 
@@ -45,12 +53,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = total.toFixed(2);
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = value.toFixed(2);
 };
 
 // update delivery charge and total Tax
@@ -75,6 +83,7 @@ const updateTotal = () => {
   const grandTotal =
     getInputValue("price") + getInputValue("delivery-charge") +
     getInputValue("total-tax");
-  document.getElementById("total").innerText = grandTotal;
+  document.getElementById("total").innerText = grandTotal.toFixed(2);
 };
+
 loadProducts();
